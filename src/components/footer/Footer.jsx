9fb57727272eac47830b4de0703be1cc6d01d3be
Lucas Silv <63755798+LucasSCSilv@users.noreply.github.com>
2022-04-21 20:11:03 -0300

    feat/footer
    feat footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './footer.css'
+import {FiInstagram} from 'react-icons/fi'
+import {SiLinkedin} from 'react-icons/si'
 
 const Footer = () => {
   return (
-    <section id='Footer'>Footer</section>
+    <footer>
+      <a href="#" className='footer__logo'>LUCAS</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">Sobre</a></li>
+        <li><a href="#experience">Experiência</a></li>
+        <li><a href="#services">Serviços</a></li>
+        <li><a href="#contact">Contato</a></li>
+      </ul>
+
+    <div className='footer__socials'>
+      <a href="https://instagram.com"><FiInstagram /></a>
+      <a href="https://linkedin.com"><SiLinkedin /></a>
+    </div>
+
+    <div className="footer__copyright">
+      <small>&copy; Lucas</small>
+    </div>
+    </footer>
   )
 }
 
