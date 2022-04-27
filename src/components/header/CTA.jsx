6498d19591b115c85850d4d319d01396fa86116c
Lucas Silv <63755798+LucasSCSilv@users.noreply.github.com>
2022-04-26 23:35:06 -0300

    feat/englishCV

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import CV from '../../assets/cv.pdf'
+import CVEN from '../../assets/cven.pdf'
 import {i18n} from '../translate/i18n'
+
+let cv;
+
+if(i18n.t('CTA.CV') === 'cv'){
+  cv = CV
+}else if(i18n.t('CTA.CV') === 'cven'){
+  cv = CVEN
+}
 
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={CV} download className='btn'>{i18n.t('CTA.downloadCV')}</a>
+        <a href={cv} download className='btn'>{i18n.t('CTA.downloadCV')}</a>
         <a href="#contact" className='btn btn-primary'>{i18n.t('CTA.letsTalk')}</a>
     </div>
   )
